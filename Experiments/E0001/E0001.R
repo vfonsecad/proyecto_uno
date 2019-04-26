cf671f7d95df01b069b2ffa7644ba1b6c16e75d8
vfonsecad <valeria.fonseca.diaz@gmail.com>
2019-04-26 06:52:53 +0200

    2019-04-25 Experiment 1, preliminary results about convergence and base function for imputation

---
+++ Experiments/E0001/E0001.R
@@ -0,0 +1,50 @@
+# --------------------------------------------------------------------
+
+# ----------- APPLY DATA IMPUTATION TECHNIQUE CONTINUOUS VARIABLES
+
+# --------------------------------------------------------------------
+
+# This experiment presents preliminary results on the convergence of imputation
+
+rm(list=ls())
+setwd("C:/Users/u0106869/Google Drive/Independent work/JULVAL")
+
+# --- Libraries and sources
+
+source("./proyecto_uno/Methodology/imputation_functions.R")
+
+library(MASS)
+
+
+# --- Apply Function
+
+# -- Matrix
+
+Sigma <- matrix(c(10,3,3,2),2,2)
+N <- 1000
+K <- 2
+Xcomplete <- mvrnorm(n = N, rep(0, K), Sigma)
+missing <- matrix(rbinom(N*K,1,0.1),nrow = N, ncol = K)
+Xmissing <- Xcomplete * (ifelse(missing==1,NA,1))
+  
+
+# -- Loop
+
+iter_vector <- seq(1,100,1)
+rmse <- matrix(0,nrow = length(iter_vector), ncol=1)
+
+kk <- 1
+
+for(iit in iter_vector){
+  
+  # -- Imputed matrix
+  
+  Ximputed <- impute_matrix(X0 = Xmissing, max_iter = iit)
+  rmse[kk,1] <- sqrt(mean((Xcomplete - Ximputed)^2))
+  kk <- kk + 1
+}
+  
+
+plot(iter_vector, rmse[,1], type = "l")
+  
+   
---
+++ Methodology/imputation_functions.R
@@ -0,0 +1,57 @@
+# --------------------------------------------------------------------
+
+# ----------------- DATA IMPUTATION TECHNIQUE CONTINUOUS VARIABLES
+
+# --------------------------------------------------------------------
+
+# This data imputation technique is based on distance of observations. It goes as follows:
+
+# Given a distance measure and X0 data matrix with missing entries:
+#   0. Initialize X = X0 with missing entries filled in randomly 
+#   1. Calculate D, distance matrix based on X of size NxN containing the distances of each couple of observations
+#   2. Calculate W, matrix of weights which is D/sum(D rowwise) so that the rows of W sum to 1
+#   3. Calculate X1 = W*X (matrix multiplication)
+#   4. Fill in the missing data with corresponding entries in X1.
+#   5. Repeat steps 1 to 4 until convergence
+
+
+impute_matrix <- function(X0,distance_method = "euclidean", X_initial = NULL, max_iter = 30){
+  
+  
+  # - Constants
+  
+  N <-dim(X0)[1]
+  K <- dim(X0)[2]
+  missing_matrix <- ifelse(is.na(X0),1,0)
+  
+  # - Iterations 
+  
+  kk <- 1
+  
+  # - Initialize
+  
+  if(is.null(X_initial)){
+    
+    X <- ifelse(is.na(X0),rnorm(1),X0)
+    
+  } else {
+    
+    X <- X_initial
+    
+  }
+  
+  # - Loop
+  
+  while(kk <= max_iter){
+    
+    D <- as.matrix(dist(X, distance_method))
+    W <- D/matrix(rep(apply(D,1,sum),N),nrow = N, byrow = FALSE)
+    X1 <- (W%*%X)*missing_matrix
+    X <- ifelse(missing_matrix == 1,X1,X0)
+    
+    kk <- kk + 1
+    
+  }
+  
+  return(X)
+}
