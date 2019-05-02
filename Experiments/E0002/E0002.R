5afdfeb5554dc3784617972b4bcf944e21a7227d
CruzJulian <cruz.julian.mauricio@gmail.com>
2019-05-02 01:09:37 -0500

    New experiment with the iris dataset

--- Experiments/E0002/E0002.R
+++ Experiments/E0002/E0002.R
@@ -20,17 +20,17 @@ source("./Methodology/imputation_functions.R")
 
 # -- Matrix
 
-Sigma <- matrix(c(10,3,3,2),2,2)
-N <- 1000
-K <- 2
-Xcomplete <- mvrnorm(n = N, rep(0, K), Sigma)
+N <- 150
+K <- 4
+Xcomplete <- as.matrix(iris[1:4])
 missing <- matrix(rbinom(N*K,1,0.1),nrow = N, ncol = K)
 Xmissing <- Xcomplete * (ifelse(missing==1,NA,1))
-  
+Xmeans <- matrix(rep(colMeans(Xcomplete), nrow(Xcomplete)), nrow(Xcomplete), byrow = TRUE)
+
 
 # -- Loop
 
-iter_vector <- seq(1,50,1)
+iter_vector <- seq(1,25,1)
 rmse <- matrix(0,nrow = length(iter_vector), ncol=1)
 rsquared <- matrix(0,nrow = length(iter_vector), ncol=1)
 
@@ -42,12 +42,10 @@ for(iit in iter_vector){
   
   Ximputed <- impute_matrix(X0 = Xmissing, max_iter = iit)
   rmse[kk,1] <- sqrt(mean((Xcomplete - Ximputed)^2))
-  rsquared[kk,1] <- 1 - sum((Xcomplete - Ximputed)^2)/sum((Xcomplete - ifelse(is.na(Xmissing),0,Xmissing))^2)
+  rsquared[kk,1] <- 1 - sum((Xcomplete - Ximputed)^2)/sum((Xcomplete - ifelse(is.na(Xmissing),Xmeans,Xmissing))^2)
   kk <- kk + 1
 }
   
 
 plot(iter_vector, rmse[,1], type = "l")
 plot(iter_vector, rsquared[,1], type = "l")
-
-   
